946f1cf4239fbdc98584102385d101521818fd1b
tirtahakimpambudhi <awebthp@gmail.com>
2023-07-16 12:00:00 +0530

    Modified src/components/sections/Footer.jsx

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,10 +1,12 @@
 import PropTypes from "prop-types";
-import { getArrayOrDefault } from "../../utils/array.js";
+
 import {
-  services as defaultServices,
   companies as defaultCompanies,
+  services as defaultServices,
   socialMedias as defaultSosMed,
 } from "../../constant/footers.jsx";
+import { getArrayOrDefault } from "../../utils/array.js";
+
 /**
  * For Footer Component
  * @param services {Array.<String>} list a service provide by company
@@ -25,11 +27,11 @@ export default function Footer({ services, companies, socialMedias }) {
     socialMedias = !(socialMedias instanceof Error) ? socialMedias : [];
   }
   return (
-    <footer className="footer bg-base-300 text-base-content p-10">
+    <footer className="footer bg-base-300 p-10 text-base-content">
       <nav>
         <h6 className="footer-title">Services</h6>
         {services.map((service, index) => (
-          <a key={index} className="link link-hover">
+          <a key={index} className="link-hover link">
             {service}
           </a>
         ))}
@@ -37,7 +39,7 @@ export default function Footer({ services, companies, socialMedias }) {
       <nav>
         <h6 className="footer-title">Company</h6>
         {companies.map((company, index) => (
-          <a key={index} href={company.href} className="link link-hover">
+          <a key={index} href={company.href} className="link-hover link">
             {company.name}
           </a>
         ))}
