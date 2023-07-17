e20267314b482066fdc1e6efa247a776575692e5
tirtahakimpambudhi <awebthp@gmail.com>
2023-07-17 12:00:00 +0530

    Modified src/components/sections/Hero.jsx

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,16 +1,22 @@
-import Navigation from "../Navigation.jsx";
 import PropTypes from "prop-types";
+
+import Navbar from "../Navbar.jsx";
 
 /**
  * For Hero Component
  * @param imageSrc {String} for image source can be used hero
+ * @param navProps {{ Brand: Element, navigations: Array.<{ name: String, href: String}>, sideNavigations: Array.<Element> }}
  * @returns {JSX.Element} return hero component
  * @constructor
  */
-export default function Hero({ imageSrc }) {
+export default function Hero({ imageSrc, navProps }) {
   return (
     <>
-      <Navigation></Navigation>
+      <Navbar
+        brand={navProps.Brand}
+        navigations={navProps.navigations}
+        sideNavigations={navProps.sideNavigations}
+      ></Navbar>
       <div
         className="hero min-h-screen bg-fixed"
         style={{
@@ -18,15 +24,15 @@ export default function Hero({ imageSrc }) {
         }}
       >
         <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-neutral-content text-center">
+        <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 dark:text-gray-300 dark:[text-shadow:_0_4px_8px_rgb(55_65_81_/_0.8)] text-neutral-950 [text-shadow:_0_4px_8px_rgb(229_231_235_/_0.8)]  text-5xl font-bold">
-              Hello there
+            <h1 className="text-gr mb-5 text-3xl font-bold [text-shadow:_0_4px_8px_rgb(55_65_81_/_0.8)] dark:text-gray-300 dark:[text-shadow:_0_4px_8px_rgb(55_65_81_/_0.8)]">
+              Improve Your Business with a Professional Website
             </h1>
-            <p className="mb-5 dark:text-gray-300 dark:[text-shadow:_0_4px_8px_rgb(55_65_81_/_0.8)] text-neutral-950 [text-shadow:_0_4px_8px_rgb(229_231_235_/_0.8)]">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+            <p className="mb-5 text-gray-300 [text-shadow:_0_4px_8px_rgb(75_85_99_/_0.8)] dark:text-gray-300 dark:[text-shadow:_0_4px_8px_rgb(55_65_81_/_0.8)]">
+              Get solutions to your business needs with a website. From
+              e-commerce to custom portfolios, we are ready to help you thrive
+              in the digital era.
             </p>
             <button className="btn btn-primary">Get Started</button>
           </div>
@@ -38,4 +44,14 @@ export default function Hero({ imageSrc }) {
 
 Hero.propTypes = {
   imageSrc: PropTypes.string.isRequired,
+  navProps: PropTypes.shape({
+    Brand: PropTypes.element.isRequired,
+    navigations: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        href: PropTypes.string.isRequired,
+      }).isRequired
+    ).isRequired,
+    sideNavigations: PropTypes.arrayOf(PropTypes.element.isRequired).isRequired,
+  }).isRequired,
 };
