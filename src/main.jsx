e509aa195f37c5bc9e4f5390de27c4eef5367380
tirtahakimpambudhi <awebthp@gmail.com>
2023-07-25 12:00:00 +0530

    Modified src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Home from "./pages/Home.jsx";
+
 import "./index.css";
+import Home from "./pages/Home.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
