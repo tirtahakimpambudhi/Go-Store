21e6171c59563bb9b3ad8cf47a6873a4c3b63005
tirtahakimpambudhi <awebthp@gmail.com>
2024-06-02 12:00:00 +0530

    :wrench: add id each section

--- src/components/sections/Feature.jsx
+++ src/components/sections/Feature.jsx
@@ -17,7 +17,7 @@ export default function Feature({ features }) {
     features = [];
   }
   return (
-    <div className="bg-base-100 py-24 sm:py-32">
+    <div id="feature" className="bg-base-100 py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-indigo-600">
